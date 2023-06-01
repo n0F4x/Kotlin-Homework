d4c3147f495d7572fd607349c5bc4c5e0698b577
Gabor Galgoczy <ggabor2002@gmail.com>
2023-06-01 23:43:59 +0200

    added initial Hook implementation

---
+++ src/main/kotlin/lib/handles/Hook.kt
@@ -0,0 +1,24 @@
+package lib.handles
+
+import kotlin.properties.Delegates
+import kotlin.reflect.KProperty
+
+typealias HookListener<T> = (
+    property: KProperty<*>, oldValue: T?, newValue: T?
+) -> Unit
+
+class Hook<T>(element: T?) {
+    var element: T? by Delegates.observable(element) { property, oldValue, newValue ->
+        listeners.forEach { it.invoke(property, oldValue, newValue) }
+    }
+
+    private val listeners = mutableListOf<HookListener<T>>()
+
+    fun addListener(listener: HookListener<T>) {
+        listeners.add(listener)
+    }
+
+    fun removeListener(listener: HookListener<T>) {
+        listeners.remove(listener)
+    }
+}
