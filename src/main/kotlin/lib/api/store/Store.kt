6839a87012f7c1bfbf8d0b302b8e66002ebacec4
Gabor Galgoczy <ggabor2002@gmail.com>
2023-06-02 20:20:23 +0200

    Store fix

--- src/main/kotlin/lib/api/store/Store.kt
+++ src/main/kotlin/lib/api/store/Store.kt
@@ -4,6 +4,7 @@ import lib.api.ecs.Entity
 import lib.api.ecs.Registry
 import lib.api.ecs.add
 import lib.api.ecs.registerType
+import kotlin.reflect.KClass
 
 class Store {
     private val states = Registry()
@@ -11,11 +12,11 @@ class Store {
 
     fun <T> add(hook: Observed<T>, tag: String): Boolean {
         states.registerType<Observed<T>>()
-        tagMap[tag] = states.add(tag to states.add(hook)) ?: return false
+        tagMap[tag] = states.add(hook) ?: return false
         return true
     }
 
-    fun get(tag: String): Any? {
+    fun get(tag: String): Pair<KClass<out Any>, Any>? {
         return states.get(tagMap[tag] ?: return null)
     }
 }
